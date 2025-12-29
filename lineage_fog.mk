d07ddbbc40985f8f30b9a6fc4b69af51f98911d2
AloozChips <aranyasahaa3@gmail.com>
2025-12-29 17:39:19 +0600

    fog: Get ready for @Evolution-X
    Signed-off-by: AloozChips <aranyasahaa3@gmail.com>

--- lineage_fog.mk
+++ lineage_fog.mk
@@ -11,6 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTS_QUICK_TAP := false
+BYPASS_CHARGE_SUPPORTED := false
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
